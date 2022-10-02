cc6dd625a0659116b3087b6275d3708fe2d89299
Michał Wójcik <michal.wojcik21@gmail.com>
2022-10-02 19:15:08 +0200

    Set up server side, adjust to typescript

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+﻿// const express = require("express");
+import express from "express";
+import dotenv from "dotenv/config";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
