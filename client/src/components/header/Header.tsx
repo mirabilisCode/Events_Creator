358ae8c5076e0b16850e71c885617933c5d1df23
Michał Wójcik <michal.wojcik21@gmail.com>
2022-10-02 17:24:41 +0200

    Add Header Component

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <div className="container header__container">
+        <h1 className="header-title">Welcome to the events store!</h1>
+      </div>
+    </header>
+  );
+}
