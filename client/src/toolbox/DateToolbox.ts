d346453343b6b3f3a5bbc065ced5d84ee19648d8
Michał Wójcik <michal.wojcik21@gmail.com>
2022-10-02 17:28:31 +0200

    Add DateToolbox.ts to the project

---
+++ client/src/toolbox/DateToolbox.ts
@@ -0,0 +1,20 @@
+﻿const getDayName = (baseDate: Date): string => {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  return days[baseDate?.getDay()];
+};
+
+const getMonthName = (baseDate: Date): string => {
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  return months[baseDate?.getMonth()];
+};
+
+const getDateWithOrder = (baseDate: Date): string => {
+  const dayOfMonthNumber = baseDate?.getDate().toString();
+  const order = dayOfMonthNumber.endsWith("1") ? "st" : dayOfMonthNumber.endsWith("2") ? "nd" : dayOfMonthNumber.endsWith("3") ? "rd" : "th";
+
+  return `${dayOfMonthNumber}${order}`;
+};
+
+export { getDayName, getMonthName, getDateWithOrder };
