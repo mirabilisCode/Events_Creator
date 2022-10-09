d7756cec21812db1ceafb8a02854fb495ed5b8d2
Michał Wójcik <michal.wojcik21@gmail.com>
2022-10-09 14:02:07 +0200

    Declare Event Model

---
+++ server/Models/EventModel.ts
@@ -0,0 +1,14 @@
+﻿import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const eventsSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  eventDate: { type: String, required: true },
+});
+
+const Event = mongoose.model("Event", eventsSchema);
+
+export default Event;
